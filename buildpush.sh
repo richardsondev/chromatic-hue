77b0c4b331136d3cc915fca1a2d795526da72ee5
Billy Richardson <contact@richardson.dev>
2023-07-13 01:57:58 -0700

    App content (#1)
    * App content

--- buildpush.sh
+++ buildpush.sh
@@ -1,15 +1,18 @@
 #!/bin/bash -e
 
-# Builds distroless Docker images with the application and pushes them to Docker Hub
-# Currently, intended to be run from a local machine
-
-APP_NAME="hello_world"
-DOCKER_HUB="YourDockerHubUsername"
+# Application name
+APP_NAME="chromatic-hue"
 
 # Step 1: Build the Docker image and push to Docker Hub
 # This builds and pushes ARM32, ARM64, and X64 images
+DOCKER_HUB="richardsondev"
 docker buildx create --use --name mybuilder || true
 docker buildx use mybuilder
 docker buildx inspect --bootstrap
-docker buildx build . --platform linux/amd64,linux/arm64,linux/arm/v7 -t $DOCKER_HUB/$APP_NAME:latest --push
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t $DOCKER_HUB/$APP_NAME:latest --push .
 
+# Step 2: Push the X64 image to Azure Container Registry
+ACR_NAME="richardsondev"
+docker tag $APP_NAME $ACR_NAME.azurecr.io/$APP_NAME:latest
+/root/bin/az acr login --name $ACR_NAME
+docker push $ACR_NAME.azurecr.io/$APP_NAME:latest
